1756e33e498093c16f14a6028fb82d7d511167d3
Baranchyck <denysbachynskiy12@gmail.com>
2026-09-09 22:49:29 +0300

    update: task2

--- ParallelComputing/Task2/Program.cs
+++ ParallelComputing/Task2/Program.cs
@@ -2,7 +2,7 @@
 
 namespace Task2
 {
-    class MatrixMultiplying
+    public class MatrixMultiplying
     {
         public static int[,] MultiplyMatrix(int[,] firstMatrix, int[,] secondMatrix)
         {

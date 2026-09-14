e3dc63f7cb8d894954c9e9f43fd383a11856db11
Baranchyck <denysbachynskiy12@gmail.com>
2026-09-14 21:18:32 +0300

    Minor fix Task2

--- ParallelComputing/Task2/Program.cs
+++ ParallelComputing/Task2/Program.cs
@@ -6,17 +6,19 @@ namespace Task2
     {
         public static int[,] MultiplyMatrix(int[,] firstMatrix, int[,] secondMatrix)
         {
-            int n = firstMatrix.GetLength(0);
-            int[,] newMatrix = new int[n, n];
+            int rows = firstMatrix.GetLength(0);
+            int inner = firstMatrix.GetLength(1);
+            int cols = secondMatrix.GetLength(1);
+            int[,] newMatrix = new int[rows, cols];
 
             Stopwatch stopWatch = Stopwatch.StartNew();
 
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < rows; i++)
             {
-                for (int k = 0; k < n; k++)
+                for (int k = 0; k < inner; k++)
                 {
                     int r = firstMatrix[i, k];
-                    for (int j = 0; j < n; j++)
+                    for (int j = 0; j < cols; j++)
                     {
                         newMatrix[i, j] += r * secondMatrix[k, j];
                     }
@@ -29,16 +31,16 @@ namespace Task2
             return newMatrix;
         }
 
-        private static Thread StartWorker(int[,] firstMatrix, int[,] secondMatrix, int[,] newMatrix, int startRow, int endRow, int n)
+        private static Thread StartWorker(int[,] firstMatrix, int[,] secondMatrix, int[,] newMatrix, int startRow, int endRow, int inner, int cols)
         {
             Thread thread = new Thread(() =>
             {
                 for (int i = startRow; i < endRow; i++)
                 {
-                    for (int k = 0; k < n; k++)
+                    for (int k = 0; k < inner; k++)
                     {
                         int r = firstMatrix[i, k];
-                        for (int j = 0; j < n; j++)
+                        for (int j = 0; j < cols; j++)
                         {
                             newMatrix[i, j] += r * secondMatrix[k, j];
                         }
@@ -52,20 +54,22 @@ namespace Task2
 
         public static int[,] MultiplyMatrixCooler(int[,] firstMatrix, int[,] secondMatrix, int potoky)
         {
-            int n = firstMatrix.GetLength(0);
-            int[,] newMatrix = new int[n, n];
+            int rows = firstMatrix.GetLength(0);
+            int inner = firstMatrix.GetLength(1);
+            int cols = secondMatrix.GetLength(1);
+            int[,] newMatrix = new int[rows, cols];
 
             Thread[] threads = new Thread[potoky];
-            int rowsPerThread = n / potoky;
+            int rowsPerThread = rows / potoky;
 
             Stopwatch stopWatch = Stopwatch.StartNew();
 
             for (int i = 0; i < potoky; i++)
             {
                 int startRow = i * rowsPerThread;
-                int endRow = (i == potoky - 1) ? n : (i + 1) * rowsPerThread;
+                int endRow = (i == potoky - 1) ? rows : (i + 1) * rowsPerThread;
 
-                threads[i] = StartWorker(firstMatrix, secondMatrix, newMatrix, startRow, endRow, n);
+                threads[i] = StartWorker(firstMatrix, secondMatrix, newMatrix, startRow, endRow, inner, cols);
             }
 
             for (int i = 0; i < potoky; i++)
@@ -88,7 +92,7 @@ namespace Task2
             {
                 for (int j = 0; j < cols; j++)
                 {
-                    matrix[i, j] = rnd.Next(1, 10); 
+                    matrix[i, j] = rnd.Next(1, 10);
                 }
             }
 
@@ -112,30 +116,33 @@ namespace Task2
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-            //int potoky = Environment.ProcessorCount;
-            int potoky = 4;
-
-            Console.WriteLine($"potoky = {potoky}");
+            Console.WriteLine("Введіть кількість потоків:");
+            int potoky = int.Parse(Console.ReadLine()!);
 
             Console.WriteLine("Прогрів: ");
             int warmupSize = 100;
             int[,] w1 = GenerateMatrix(warmupSize, warmupSize);
             int[,] w2 = GenerateMatrix(warmupSize, warmupSize);
-
+            int pot = 4;
             MultiplyMatrix(w1, w2);
-            MultiplyMatrixCooler(w1, w2, potoky);
+            MultiplyMatrixCooler(w1, w2, pot);
 
             Console.WriteLine("----------------------------------");
 
-            List<int> n = [200, 400, 800, 1600, 2400];
+            List<int> rowsList = [10, 50, 100, 200, 300, 400, 800, 1600, 2000, 2400];
+            List<int> colsList = [10, 50, 150, 200, 500, 400, 1000, 1600, 2000, 3000];
 
-            for (int i = 0; i < n.Count; i++)
+            Console.WriteLine($"potoky = {potoky}");
+
+            for (int i = 0; i < rowsList.Count; i++)
             {
-                int size = n[i];
-                Console.WriteLine($"Розмір: {size} x {size}");
+                int rows = rowsList[i];
+                int cols = colsList[i];
 
-                int[,] m1 = GenerateMatrix(size, size);
-                int[,] m2 = GenerateMatrix(size, size);
+                Console.WriteLine($"Розмір: {rows} x {cols}");
+
+                int[,] m1 = GenerateMatrix(rows, cols);
+                int[,] m2 = GenerateMatrix(cols, rows);
 
                 var resSync = MultiplyMatrix(m1, m2);
                 var resAsync = MultiplyMatrixCooler(m1, m2, potoky);
